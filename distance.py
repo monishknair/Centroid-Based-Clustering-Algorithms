822af190cc337e58b8892ba1792abbbb7dcaabc5
monishknair <monishknair@outlook.com>
2017-10-30 15:44:32 +0530

    initial plan

---
+++ distance.py
@@ -0,0 +1,19 @@
+class Euclid(object):
+	def __init__(self):
+		pass
+	def __call__():
+		pass
+
+class Manhattan():
+	def __init__():
+		pass
+	def __call__():
+		pass
+
+class Minkowski():
+	def __init__():
+		pass
+	def __call__():
+		pass
+
+
---
+++ measure/distance.py
@@ -0,0 +1,24 @@
+class Euclid(object):
+	def __init__(self):
+		pass
+	def __call__():
+		pass
+
+class Manhattan():
+	def __init__():
+		pass
+	def __call__():
+		pass
+
+class Minkowski():
+	def __init__():
+		pass
+	def __call__():
+		pass
+
+
+class chebyshev():
+	def __init__():
+		pass
+	def __calll__():
+
